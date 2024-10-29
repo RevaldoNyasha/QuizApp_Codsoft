6b9f6257f92346fbd403494dab9109265a9d1fad
nyash <nyasharevaldo@gamil.com>
2024-10-29 00:57:19 -0700

    final version

---
+++ app/src/main/java/com/example/appquiz/MainActivity.java
@@ -0,0 +1,106 @@
+package com.example.appquiz;
+
+import android.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends  AppCompatActivity implements  View.OnClickListener{
+    TextView questionTextView;
+    TextView totalQuestionTextView;
+    Button ansA,ansB,ansC,ansD;
+    Button btn_submit;
+
+    int score=0;
+    int totalQuestion =QuestionAnswer1.question.length;
+    int currentQuestionIndex = 0;
+    String selectedAnswer="";
+
+    @Override
+
+  protected void onCreate(Bundle savedInstanceState){
+       super.onCreate(savedInstanceState);
+       setContentView(R.layout.activity_main);
+       totalQuestionTextView = findViewById(R.id.Total_questions);
+       questionTextView = findViewById(R.id.questions);
+       ansA = findViewById(R.id.ans_a);
+       ansB = findViewById(R.id.ans_b);
+       ansC = findViewById(R.id.ans_c);
+       ansD = findViewById(R.id.ans_d);
+       btn_submit = findViewById(R.id.btn_submit);
+
+          ansA.setOnClickListener(this);
+          ansB.setOnClickListener(this);
+          ansC.setOnClickListener(this);
+          ansD.setOnClickListener(this);
+          btn_submit.setOnClickListener(this);
+
+       totalQuestionTextView.setText("Total question : "+totalQuestion);
+
+       loadNewQuestion();
+  }
+  private void loadNewQuestion(){
+      if(currentQuestionIndex == totalQuestion){
+          finishQuiz();
+          return;
+      }
+      questionTextView.setText(QuestionAnswer1.question[currentQuestionIndex]);
+      ansA.setText(QuestionAnswer1.choices[currentQuestionIndex][0]);
+      ansB.setText(QuestionAnswer1.choices[currentQuestionIndex][1]);
+      ansC.setText(QuestionAnswer1.choices[currentQuestionIndex][2]);
+      ansD.setText(QuestionAnswer1.choices[currentQuestionIndex][3]);
+  }
+
+  private void finishQuiz(){
+      String passStatus;
+      if(score>=totalQuestion*0.6){
+          passStatus="Passed";
+      }else{
+          passStatus="failed";
+      }
+      new AlertDialog.Builder(this)
+              .setTitle(passStatus)
+              .setMessage("Your Score is "+score+"Out of"+totalQuestion)
+              .setPositiveButton("Restart",((dialog, i) -> restartQuiz() ))
+              .setCancelable(false)
+              .show();
+
+  }
+  private void restartQuiz(){
+      score=0;
+      currentQuestionIndex=0;
+      loadNewQuestion();
+  }
+  @Override
+    public void onClick(View view){
+      ansA.setBackgroundColor(Color.WHITE);
+      ansB.setBackgroundColor(Color.WHITE);
+      ansC.setBackgroundColor(Color.WHITE);
+      ansD.setBackgroundColor(Color.WHITE);
+
+      Button clickedButton = (Button) view;
+
+      if(clickedButton.getId() == R.id.btn_submit){
+          if(!selectedAnswer.isEmpty()){
+              if(selectedAnswer.equals(QuestionAnswer1.correctAnswer[currentQuestionIndex])){
+                  score++;
+              }else{
+                  clickedButton.setBackgroundColor(Color.MAGENTA);
+              }
+              currentQuestionIndex++;
+              loadNewQuestion();
+
+          }else{
+
+          }
+
+      }else{
+          selectedAnswer=clickedButton.getText().toString();
+          clickedButton.setBackgroundColor(Color.YELLOW);
+      }
+  }
+
+}
---
+++ app/src/main/java/com/example/appquiz/QuestionAnswer1.java
@@ -0,0 +1,25 @@
+package com.example.appquiz;
+
+public class QuestionAnswer1 {
+    public static String question[] = {
+            "What is the capital city of zimbabwe ?",
+            "Which famous waterfall is located between Zimbabwe and Zambia?",
+            "When did Zimbabwe gain independence?",
+            "Who was Zimbabwe’s first president?",
+            "What national park in Zimbabwe is famous for its wildlife?"
+    };
+    public static String choices [][]={
+            {"Harare", "Bulawayo", "Gweru", "Mutare"},
+            {"Victoria Falls", "Niagara Falls", "Angel Falls", "Iguazu Falls"},
+            {"1980", "1975", "1965", "1990"},
+            {"Canaan Banana", "Robert Mugabe", "Emmerson Mnangagwa", "Morgan Tsvangirai"},
+            {"Hwange National Park", "Yellowstone National Park", "Kruger National Park", "Serengeti National Park"},
+    };
+    public static String correctAnswer []={
+            "Harare",
+            "Victoria Falls",
+            "1980",
+            "Canaan Banana",
+            "Hwange National Park"
+    };
+}
